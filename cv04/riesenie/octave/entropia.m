fb3280956dae4e28c7bf2d5bac6b0be8229f63da
Dana Škorvánková <danaskorvankova@gmail.com>
2021-03-11 10:39:41 +0100

    Add files via upload

---
+++ cv04/riesenie/octave/entropia.m
@@ -0,0 +1,8 @@
+function res = entropia(X)
+  [categories, ~, idx] = unique(X);
+  counts = accumarray(idx, 1);
+  p = counts/length(X);
+  p2(p==0) = 0;
+  p2 = log2(p);
+  res = - sum(p .* p2);
+endfunction
---
+++ cv04/riesenie/octave/podm_entropia.m
@@ -0,0 +1,8 @@
+function res = podm_entropia(Y, X) # H(Y|X)
+  [categories, ~, idx] = unique(X);
+  counts = accumarray(idx, 1);
+  res = 0;
+  for i=1:length(categories)
+   res += sum(counts(i)/length(X) * entropia(Y(strcmp(X,categories{i}))));
+  endfor
+endfunction
